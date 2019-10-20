737a3c421ef8f72816143711e0eaa56b25b79479
HAOYUAN ZHANG <zhy21345@gmail.com>
2019-10-21 04:59:53 +0800

    Official Scene

---
+++ GJ_C2/Assets/Scripts/KillLadder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillLadder : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ladder")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
