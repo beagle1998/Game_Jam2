982d849c0c191d7a5571c7740d9ab37f2fc10d11
Tyler Mun <tajun09@gmail.com>
2019-10-19 20:19:26 -0700

    Continue_not_yet

---
+++ GJ_C2/Assets/Die.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        
+    }
+}
