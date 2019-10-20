61b6736e235572e8e68a17323fbb9a8c161a048c
Randolph Teh <randogteh@gmail.com>
2019-10-20 14:34:32 -0700

    Added Jump Sounds and Button Sounds
    jumpy jumpy!

---
+++ GJ_C2/Assets/buttonFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFX : MonoBehaviour
+{
+    public AudioSource mySounds;
+    public AudioClip buttonHover;
+    public AudioClip buttonClick;
+    public void HoverSound()
+    {
+        mySounds.PlayOneShot(buttonHover);
+    }
+
+    // Update is called once per frame
+    public void ClickSound()
+    {
+        mySounds.PlayOneShot(buttonClick);
+    }
+}
