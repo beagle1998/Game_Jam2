6b7a9dade09b95a4268aa44d6365d92adb1ee2f2
Randolph Teh <randogteh@gmail.com>
2019-10-20 11:19:08 -0700

    ADDED START MENU WITH FUNCTIONALITY
    MAIN MENU WORKS (NEED TO ADD GAME NAME AND/OR IMAGES)
    
    also goes to "Randolph" scene with black cat.

---
+++ GJ_C2/Assets/Scripts/Menu Scripts/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        else
+        {
+            QuitGame();
+        }
+
+    }
+}
