0610b3f8c6c1878b00a8d7f7d5e6cfb72794a034
Tyler Mun <tajun09@gmail.com>
2019-10-20 17:00:22 -0700

    Timer

---
+++ GJ_C2/Assets/Scripts/Timer_Time.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer_Time : MonoBehaviour
+{
+    private float time=0f;
+    [SerializeField] private Transform player;
+    [SerializeField] private int location;
+    [SerializeField] private Text timer_counter;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+       timer_counter.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    timer_counter.gameObject.SetActive(true);
+        time = time + Time.deltaTime;
+        Debug.Log(time);
+        timer_counter.text = "Time Wasted: " + time.ToString();
+        if (player.transform.position.x <= location)
+        {
+            timer_counter.gameObject.SetActive(true);
+            
+        }
+       
+
+    }
+}
